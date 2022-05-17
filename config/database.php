29ba3be8efa4177f609ff3605badd6252d31e49e
miisan-miisan <77946692+miisan-miisan@users.noreply.github.com>
2022-05-17 13:19:50 +0900

    charsetをutf8_unicode_ciに

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
             'password' => $db['pass'],
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
